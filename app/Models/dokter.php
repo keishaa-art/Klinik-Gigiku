9d557772c08cfbe8fb86ada504dc6d71b83cb12f
BM-LEOZARD <bgs040808@gmail.com>
2025-07-24 08:38:52 +0700

    code

--- app/Models/dokter.php
+++ app/Models/dokter.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class dokter extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'dokters';
+    protected $fillable = [
+        'nip',
+        'nama',
+        'spesialis',
+        'no_hp',
+        'email',
+        'alamat',
+    ];
 }
