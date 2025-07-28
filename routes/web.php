cd57671cc078b566206358ee6393523fa797cc59
keishaa-art <keishaathaya996@gmail.com>
2025-07-28 09:28:47 +0700

    crud basic pemeriksaan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\PemeriksaanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('pemeriksaan', PemeriksaanController::class);
