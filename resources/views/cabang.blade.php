5e0c39ea53170217c973ddc3f7bee8b6baed9664
boyajvv <boyarizkyagung5@gmail.com>
2025-07-31 10:36:40 +0700

    add cabang

---
+++ resources/views/cabang.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.index')
+
+@section('konten')
+  <!-- content -->
+  <div class="w-full bg-gradient-to-r from-[#FFE6E1] to-[#F0BAAF] shadow-md z-50 mt-[50px] p-8 min-h-[250px] background-color-[#F0BAAF]">
+    <h1 class=" font-['Istok_Web'] font-semibold text-5xl mt-[40px] text-[#C04C4C] ">Gigiku Dental Clinic</h1>
+    <h2 class="font-['Istok_Web'] font-semibold text-[#C04C4C]">
+  </div>
+
+    <div class="w-full mt-[2px] p-8">
+      <h1 class="font-['Istok_Web'] font-semibold text-[#C04C4C] text-2xl">Pilih Dokter : </h1>
+  </div>
+
+  @endsection
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/reservasi', function () {
     return view('reservasi');
 });
+Route::get('/1', function () {
+    return view('cabang');
+});
