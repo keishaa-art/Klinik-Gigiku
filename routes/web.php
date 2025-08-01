17e78c60be5a2a4c94add2ab3f61097beb98bc07
keishaa-art <keishaathaya996@gmail.com>
2025-08-01 13:58:35 +0700

    nambah cabang

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\PemeriksaanController;
+use App\Http\Controllers\Admin\CabangController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::resource('pemeriksaan', PemeriksaanController::class);
+Route::resource('cabang', CabangController::class);
